077562a90f3d3e418f55432bbae53e1424e6e7e7
Kazzuki <madakazuki@icloud.com>
2020-12-01 18:24:25 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import logging
+import sys
+
+import settings
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+if __name__ == '__main__':
+    print(settings.account_id)
+    print(settings.access_token)
+
